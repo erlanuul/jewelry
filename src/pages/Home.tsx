145a6d04dfe6ef4f59df5da7c77bdd33d543f8b5
erlanuul <erlauul@gmail.com>
2024-04-03 10:40:04 +0600

    work with analytics statistic

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -56,21 +56,21 @@ export default function Home() {
                 <h1 className="text-[#2A2826] text-[42px] font-[800]">Главная</h1>
             </div>
 
-            <div className="w-full flex justify-start items-start gap-[20px]">
-                {analyticsFinance.loading
-                    ?
+            {analyticsFinance.loading
+                ?
+                <>
+                    <div className="w-full flex justify-start items-start gap-[20px] mb-[80px]">
+                        <Skeleton variant="rectangular" width={'20%'} height={248}/>
+                        <Skeleton variant="rectangular" width={'80%'} height={248}/>
+                    </div>
+                </>
+                : analyticsFinance.error
+                    ? analyticsFinance.error.message
+                    :
                     <>
-                        <div className='w-full flex flex-col justify-start items-center mb-[39px]'>
-                            <Skeleton variant="rectangular" width={'40%'} height={30}/>
-                        </div>
-                        <Skeleton variant="rectangular" width={'100%'} height={500}/>
-                    </>
-                    : analyticsFinance.error
-                        ? analyticsFinance.error.message
-                        :
-                        <>
+                        <div className="w-full flex justify-start items-start gap-[20px] mb-[80px]">
                             {[...analyticsFinance.result?.data.finance].filter((item: any) => !item.cashless).map((index: number) => (
-                                <div className="p-[14px] rounded-[10px] bg-white min-h-[238px] flex flex-col justify-between items-center" key={index}>
+                                <div className="p-[14px] rounded-[10px] bg-white min-h-[238px] flex flex-col justify-between items-center shadow-md" key={index}>
                                     <div className="w-full flex justify-between items-center gap-[20px] pb-[10px]"
                                          style={{borderBottom: '1px solid #576ED0'}}
                                     >
@@ -89,7 +89,7 @@ export default function Home() {
                                 </div>
                             ))}
 
-                            <div className="p-[14px] rounded-[10px] bg-white flex flex-col justify-items-start items-start">
+                            <div className="p-[14px] rounded-[10px] bg-white flex flex-col justify-items-start items-start shadow-md">
                                 <div className="w-full flex justify-start items-center gap-[20px] pb-[10px] mb-[30px]"
                                      style={{borderBottom: '1px solid #576ED0'}}
                                 >
@@ -128,98 +128,125 @@ export default function Home() {
                                     ))}
                                 </div>
                             </div>
-                        </>
-                }
+                        </div>
 
-                <Modal open={modal.open} onClose={() => setModal(modalInitialValues)}>
-                    <form onSubmit={handleFormSubmit} className='mainModal'>
-                        <h1 className='text-[#2A2826] text-[24px] font-[700]'>
-                            Снятие
-                        </h1>
-                        <div className="w-full flex flex-col justify-start items-center gap-[50px]">
-                            <div className='w-full flex flex-col gap-[10px] pb-[10px]' style={{borderBottom: '1px solid #CED0D2'}}>
-                                <p className="text-[#576ED0] text-[14px] font-[500]">{modal.values.name}</p>
-                                <p className="text-[#576ED0] text-[20px] font-[600]">{modal.values.total_sum} сом</p>
+                        <div className="w-full flex flex-col justify-start items-star">
+                            <h3 className="text-[#2A2826] text-[24px] font-[700] mb-[40px]">
+                                Статистика
+                            </h3>
+                            <div className="w-full grid grid-cols-4 gap-[20px]">
+                                <div className="w-full p-[14px] rounded-[10px] bg-white flex flex-col justify-start items-center shadow-md">
+                                    <div className="w-full flex justify-between items-center pb-[10px] mb-[24px]"
+                                         style={{borderBottom: '1px solid #576ED0'}}
+                                    >
+                                        <p className="text-[#576ED0] text-[16px] font-[600] text-nowrap">Продажи</p>
+                                    </div>
+                                    <div className="w-full flex justify-between items-start gap-[20px] mb-[40px]">
+                                        <div className="flex flex-col justify-start items-start gap-[4px]">
+                                            <p className="text-[#3E3C3A] text-[20px] font-[600]">10 000</p>
+                                            <p className="text-[#6E6C6A] text-[10px] font-[500]">Количество продаж</p>
+                                        </div>
+
+                                        <div className="flex flex-col justify-start items-start gap-[4px]">
+                                            <p className="text-[#3E3C3A] text-[20px] font-[600]">10 000</p>
+                                            <p className="text-[#6E6C6A] text-[10px] font-[500]">Количество продаж</p>
+                                        </div>
+                                    </div>
+                                    <div className="w-full flex justify-start items-center gap-[20px]"></div>
+                                </div>
                             </div>
-                            <div className='w-full grid grid-cols-2 gap-[30px]'>
-                                <CustomTextField
-                                    fullWidth
-                                    label='Сумма'
-                                    placeholder='Сумма'
-                                    required
-                                    type='number'
-                                    value={modal.values.amount}
-                                    error={modal.validations.errors.amount}
-                                    helperText={modal.validations.messages.amount}
-                                    onChange={(event) => {
-                                        if(parseInt(event.target.value) > modal.values.total_sum){
-                                            setModal({
-                                                ...modal,
-                                                values: {
-                                                    ...modal.values,
-                                                    amount: event.target.value,
-                                                },
-                                                validations: {
-                                                    ...modal.validations,
-                                                    messages:{
-                                                        ...modal.validations.messages,
-                                                        amount: 'Недостаточно средств!',
-                                                    },
-                                                    errors:{
-                                                        ...modal.validations.errors,
-                                                        amount: true,
-                                                    }
-                                                }
-                                            })
-                                        }else{
-                                            setModal({
-                                                ...modal,
-                                                values: {
-                                                    ...modal.values,
-                                                    amount: event.target.value,
-                                                },
-                                                validations: {
-                                                    ...modalInitialValues.validations,
-                                                }
-                                            })
-                                        }
-                                    }}
-                                />
-                                <CustomTextField
-                                    fullWidth
-                                    label='Примечание'
-                                    placeholder='Примечание'
-                                    required
-                                    value={modal.values.note}
-                                    error={modal.validations.errors.note}
-                                    helperText={modal.validations.messages.note}
-                                    onChange={(event) => {
+                        </div>
+                    </>
+            }
+
+            <Modal open={modal.open} onClose={() => setModal(modalInitialValues)}>
+                <form onSubmit={handleFormSubmit} className='mainModal'>
+                    <h1 className='text-[#2A2826] text-[24px] font-[700]'>
+                        Снятие
+                    </h1>
+                    <div className="w-full flex flex-col justify-start items-center gap-[50px]">
+                        <div className='w-full flex flex-col gap-[10px] pb-[10px]' style={{borderBottom: '1px solid #CED0D2'}}>
+                            <p className="text-[#576ED0] text-[14px] font-[500]">{modal.values.name}</p>
+                            <p className="text-[#576ED0] text-[20px] font-[600]">{modal.values.total_sum} сом</p>
+                        </div>
+                        <div className='w-full grid grid-cols-2 gap-[30px]'>
+                            <CustomTextField
+                                fullWidth
+                                label='Сумма'
+                                placeholder='Сумма'
+                                required
+                                type='number'
+                                value={modal.values.amount}
+                                error={modal.validations.errors.amount}
+                                helperText={modal.validations.messages.amount}
+                                onChange={(event) => {
+                                    if(parseInt(event.target.value) > modal.values.total_sum){
                                         setModal({
                                             ...modal,
                                             values: {
                                                 ...modal.values,
-                                                note: event.target.value,
+                                                amount: event.target.value,
+                                            },
+                                            validations: {
+                                                ...modal.validations,
+                                                messages:{
+                                                    ...modal.validations.messages,
+                                                    amount: 'Недостаточно средств!',
+                                                },
+                                                errors:{
+                                                    ...modal.validations.errors,
+                                                    amount: true,
+                                                }
                                             }
                                         })
-                                    }}
-                                />
-                            </div>
-                        </div>
-
-                        <div className='w-1/2'>
-                            <LoadingButton
+                                    }else{
+                                        setModal({
+                                            ...modal,
+                                            values: {
+                                                ...modal.values,
+                                                amount: event.target.value,
+                                            },
+                                            validations: {
+                                                ...modalInitialValues.validations,
+                                            }
+                                        })
+                                    }
+                                }}
+                            />
+                            <CustomTextField
                                 fullWidth
-                                variant='contained'
-                                loading={modal.requested}
-                                disabled={modal.requested}
-                                type='submit'
-                            >
-                                Готово
-                            </LoadingButton>
+                                label='Примечание'
+                                placeholder='Примечание'
+                                required
+                                value={modal.values.note}
+                                error={modal.validations.errors.note}
+                                helperText={modal.validations.messages.note}
+                                onChange={(event) => {
+                                    setModal({
+                                        ...modal,
+                                        values: {
+                                            ...modal.values,
+                                            note: event.target.value,
+                                        }
+                                    })
+                                }}
+                            />
                         </div>
-                    </form>
-                </Modal>
-            </div>
+                    </div>
+
+                    <div className='w-1/2'>
+                        <LoadingButton
+                            fullWidth
+                            variant='contained'
+                            loading={modal.requested}
+                            disabled={modal.requested}
+                            type='submit'
+                        >
+                            Готово
+                        </LoadingButton>
+                    </div>
+                </form>
+            </Modal>
         </>
     );
 }
